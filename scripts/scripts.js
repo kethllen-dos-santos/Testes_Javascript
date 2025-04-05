4cfca80a7605b21197098339cc51276968f654ce
Kethllen dos Santos <dkethllen@gmail.com>
2025-04-05 16:05:31 -0400

    Adicionada funcionalidade na página "Manipulando estilos"

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -51,3 +51,27 @@ function distribui_caractere() {
 		document.getElementById("caractere_especial").value += (" " + caractere)
 	}
 }
+
+
+function mudarCorFundo(cor) {
+	document.getElementById("quadrado").style.background = cor
+}
+
+function mudarTamanho(tamanho){
+
+	switch(tamanho) {
+	case "100":
+		document.getElementById("quadrado").style.width = "100px"
+		document.getElementById("quadrado").style.height = "100px"
+		break
+	case "150":
+		document.getElementById("quadrado").style.width = "150px"
+		document.getElementById("quadrado").style.height = "150px"
+		break
+	case "200":
+		document.getElementById("quadrado").style.width = "200px"
+		document.getElementById("quadrado").style.height = "200px"
+		break
+	}
+	
+}
